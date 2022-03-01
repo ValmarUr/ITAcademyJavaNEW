2cf11c3376112b1ae2bd72aa8508375110d5c858
Juanem <oliver.santquirze@gmail.com>
2022-03-01 17:53:42 +0100

    Swagger group correction

--- s402n1/src/main/java/com/itacademy/crud/h2/configuration/SwaggerConfig.java
+++ s402n1/src/main/java/com/itacademy/crud/h2/configuration/SwaggerConfig.java
@@ -20,7 +20,7 @@ public class SwaggerConfig {
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
-				.groupName("employee-api-v1")
+				//.groupName("employee-api-v1")
 				.select()
 				//.apis(RequestHandlerSelectors.any())
 				//.paths(PathSelectors.any())
